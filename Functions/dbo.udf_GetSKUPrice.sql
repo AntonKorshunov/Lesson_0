9d21f84d3404675473e68452054fed951c1187e7
AntonKorshunov <89854365+AntonKorshunov@users.noreply.github.com>
2023-02-19 14:21:20 +0300

    Create Functions

---
+++ Functions/dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,16 @@
+IF OBJECT_ID('dbo.udf_GetSKUPrice') IS NOT NULL DROP FUNCTION dbo.udf_GetSKUPrice;
+GO
+CREATE FUNCTION dbo.udf_GetSKUPrice
+(
+	@ID_SKU INT
+)
+RETURNS DECIMAL(18, 2)
+AS
+BEGIN
+	DECLARE @ProdVal DECIMAL(18, 2)
+	SELECT @ProdVal =  SUM([Value])/SUM(Quantity)
+	FROM Basket
+	GROUP BY ID_SKU
+	HAVING ID_SKU = @ID_SKU
+	RETURN @ProdVal
+END;
