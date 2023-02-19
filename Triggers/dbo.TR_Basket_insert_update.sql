e094ce7b962e0d7ea99855dcece8f0ec23aaa3b8
AntonKorshunov <89854365+AntonKorshunov@users.noreply.github.com>
2023-02-19 22:03:22 +0300

    Update dbo.TR_Basket_insert_update.sql

--- Triggers/dbo.TR_Basket_insert_update.sql
+++ Triggers/dbo.TR_Basket_insert_update.sql
@@ -1,15 +1,11 @@
-IF OBJECT_ID('dbo.TR_Basket_insert_update') IS NOT NULL 
-	DROP TRIGGER dbo.TR_Basket_insert_update;
-GO
-CREATE TRIGGER dbo.TR_Basket_insert_update
+CREATE OR ALTER TRIGGER dbo.TR_Basket_insert_update
 ON dbo.Basket
 AFTER INSERT
 AS
 UPDATE dbo.Basket
-SET DiscountValue = CASE WHEN Basket.ID_SKU IN (SELECT ID_SKU 
+SET DiscountValue = [Value]*0.05
+WHERE Basket.ID_SKU IN (SELECT ID_SKU 
 						FROM inserted 
 						GROUP BY ID_SKU
-						HAVING COUNT(*) > 1)
-			 THEN [Value]*0.05
-			 ELSE 0
-		    END;
+						HAVING COUNT(*) > 1
+						)
